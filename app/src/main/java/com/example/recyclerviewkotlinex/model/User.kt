5d2cd91aa727eafc6341cec98446de84f06fc794
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-06-22 17:31:26 +0300

    Adding User data class

---
+++ app/src/main/java/com/example/recyclerviewkotlinex/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerviewkotlinex.model
+
+data class User(
+    val id: Long,
+    val photo: String,
+    val name: String,
+    val company: String,
+)
+
---
+++ app/src/main/java/com/example/recyclerviewkotlinex/model/UserService.kt
@@ -0,0 +1,4 @@
+package com.example.recyclerviewkotlinex.model
+
+class UserService {
+}
